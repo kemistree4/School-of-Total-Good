0ca5438ce9d42a617feb4f28cad5e90ed61515a3
Rikeem Sholes <rikeem.sholes@gmail.com>
2019-10-29 19:38:11 -0700

    initial commit of README

---
+++ Python tutorial/addition_and_input.py
@@ -0,0 +1,11 @@
+init_number = eval(input("Please input a number"))
+new_number = eval(input("Please input another number"))
+
+if init_number or new_number != -1:
+    sum = init_number + new number
+    print("The sum is " + str(sum))
+    count = 2
+    average = sum/count
+    print("The average is " + str(init_number+new_number/count))
+    count == count + 1
+    
---
+++ README.md
@@ -0,0 +1,5 @@
+# School of Total Good
+
+- Python practice
+- Machine Learning Exercises
+- Data Science Capstone Projects
