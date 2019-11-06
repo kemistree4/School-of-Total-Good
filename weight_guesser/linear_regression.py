37d38db5b6d627b9cb962364b1c190bb792a56c6
Rikeem Sholes <rikeem.sholes@gmail.com>
2019-11-05 19:41:07 -0800

    Week 3 assignments

---
+++ weight_guesser/linear_regression.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 19:03:36 2019
+
+@author: Rikeem
+
+
+--Difference between a classifier and a regression
+--Find other regression models (based on linear regression)
+--Install scikit-learn
+--import sklearn
+--learn about (reg.coef_) attribute in Linear Regression
+--also for (reg.intercept_)
+--investigate sklearn.linear_model.LinearRegression()
+--use git to push to github
+--git config --global user.email
+--ssh-keygen
+--cat ~/.ssh/id_rsa.pub
+--debounce logic/ hysteresis loop python
+"""
+
+import sklearn
