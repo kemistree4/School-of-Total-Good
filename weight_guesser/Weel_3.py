7a51a727efb273771fadc5623c088b7f654cdbb0
kemistree4 <rikeem.sholes@gmail.com>
2019-11-28 08:57:02 -0800

    Data Frame

--- weight_guesser/Weel_3.py
+++ weight_guesser/Weel_3.py
@@ -7,9 +7,10 @@ Created on Thu Nov 28 07:36:39 2019
 """
 
 import pandas as pd
-from pandas import DataFrame
-lo = pd.read_html("https://en.wikipedia.org/wiki/Largest_organisms")
-lo = lo[0]
-df = DataFrame(lo)
 
-print(df)
+#Importing all html charts on Wikipedia
+lo = pd.read_html("https://en.wikipedia.org/wiki/Largest_organisms")
+
+#Selecting first chart from all of them and converting to dataframe
+lo = lo[0]
+
