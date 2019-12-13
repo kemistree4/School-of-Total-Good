6cf0471bb7a93d4bd95a362872957c00a77a967b
kemistree4 <rikeem.sholes@gmail.com>
2019-12-13 10:29:49 -0800

    updating School folder

--- weight_guesser/Weel_3.py
+++ weight_guesser/Weel_3.py
@@ -14,5 +14,16 @@ lo = pd.read_html("https://en.wikipedia.org/wiki/Largest_organisms")
 #Selecting first chart from all of them and converting to dataframe
 lo = lo[0]
 
-lo.to_csv(r'/home/kemistree4/code/School-of-Total-Good/weight_guesser/Week_3.csv')
-lo.to_html(r'/home/kemistree4/code/School-of-Total-Good/weight_guesser/Week_3.html')
+#lo.to_csv(r'/home/kemistree4/code/School-of-Total-Good/weight_guesser/Week_3.csv')
+#lo.to_html(r'/home/kemistree4/code/School-of-Total-Good/weight_guesser/Week_3.html')
+
+column_names = lo.columns
+print(column_names)
+
+lo.dtypes
+lo.index.values
+
+columns_to_drop = [column_names[i] for i in [0]] #Identifies the column I want to remove
+
+lo.drop(columns_to_drop, inplace=True, axis=1) #Removes the rank column, it's useless for analysis
+
