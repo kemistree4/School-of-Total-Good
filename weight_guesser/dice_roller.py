0b00a89e28feea870e175bb412dc5ffb4427e722
kemistree4 <rikeem.sholes@gmail.com>
2020-01-09 05:23:04 -0800

    Updating dice roller

--- weight_guesser/dice_roller.py
+++ weight_guesser/dice_roller.py
@@ -8,6 +8,11 @@ Created on Mon Dec  2 09:49:01 2019
 - use df.sum() and df.value_counts() to compute the probability of every possible sum (try to do this without a for loop)
 - add doctest to confirm values in dataframe --use hist -o -p and copy the entire dataframe output
 - Plot histogram of sums (5 hist for 1,2,3,4, and 5 dice)
+- Read up on central limit theorem, normal curve (Gaussian)
+- calculate z-score
+- calculate mean (.mean()) and standard deviation (.std()) of 5 dice sum
+- what's the probability of the five dice rolling a value of 5 (five ones)
+ 
 >>> type(generate_possibilities())
 DataFrame
 >>> generate_possibilites().shape
@@ -28,32 +33,38 @@ def generate_possibilities():
 
 import pandas as pd
 from itertools import product
+from matplotlib import pyplot as plt
+#number = int(input('How many die(dice) would you like to roll?'))
 
-number = int(input('How many die(dice) would you like to roll?'))
-
-def get_num_list():
+def get_num_list(number):
     number_list = []
-    
     for i in range(0, number): #loop that runs the number of times the user has input 
         number_list.append("Rolled_Die_" + str(i + 1))
     return number_list
-
+        
 #Create a function that generates a list of dice possibilities and turns that list into a dataframe 
-def roll_die():
-    dice = [1,2,3,4,5,6]
-    number_list = get_num_list()
-    df = pd.DataFrame(list(product(dice, repeat=number)), columns=number_list) #list of all possibilities for two six-sided dice
-    df.loc[:,'Sum']= df.sum(axis=1)
-#    dp = df['Sum'].value_counts(normalize=True)
-#    dp1 = df['Sum'].value_counts(normalize=False)
-    #print(dp)
-    #print(dp1.plot())
+def roll_die(number):
+    for i in range(0,number):
+        dice = [1,2,3,4,5,6]
+        number_list = get_num_list(number)
+        df = pd.DataFrame(list(product(dice, repeat= number)), columns=number_list) #list of all possibilities for two six-sided dice
+        df['Sum'] = df.sum(axis=1)
+        #dp = df['Sum'].value_counts(normalize=True)
+        #df_1 = df['Sum'].value_counts(normalize=False)
     return df
-
-
+        
 def main():
-    df = roll_die()
-    print(df)
-
+    figs = []
+    for i in range(5):
+        number = i + 1
+        df = roll_die(number)
+        print(df)
+        fig = df['Sum'].hist(bins=6 * (i + 1) - 1) 
+        plt.savefig(f"dice{i}.jpg")
+        figs.append(fig)
+        plt.clf()
+    return figs
+        
 if __name__ == "__main__":
-    main()
+    figs = main()
+    
