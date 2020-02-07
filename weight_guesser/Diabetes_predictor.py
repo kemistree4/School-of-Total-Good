54b4d3e7f42b4eb609379e95291f2206cd982ffe
kemistree4 <rikeem.sholes@gmail.com>
2020-02-07 08:41:27 -0800

    Starting diabetes assignment

---
+++ weight_guesser/Diabetes_predictor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 21:32:48 2020
+
+@author: kemistree4
+"""
+
+"""Predict the severity of diabetes for a set of patients
+-"Severity" will be one of columns (target variable)
+-Regression (Linear and then Ridge/Lasso/SGD)
