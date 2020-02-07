4e6349a4bb95325a845b83eddf322f5f0d76caa3
kemistree4 <rikeem.sholes@gmail.com>
2020-02-07 08:40:03 -0800

    Finished coding with graphs and user input

--- weight_guesser/Multivariate_weight_guesser.py
+++ weight_guesser/Multivariate_weight_guesser.py
@@ -23,19 +23,20 @@ df_gender.drop('Gender_Female',axis=1,inplace=True) #Drops the female column
 
 #Seperate dependent data column in dataframe from independent data (attributes and labels)
 X = df_gender[['Height','Gender_Male']]
-y = df_gender['Weight'].values.reshape(-1,1)
+y = df_gender['Weight'].values.reshape(-1,1) #Look into transpose. Careful with reshape
 
 #Splits the data so that 80% of the data is the training set and 20% is the test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0) 
 
 #Import the LinearRegression class and call the fit() method for training data
 regressor = LinearRegression()
-regressor.fit(df_gender[['Height','Gender_Male']], df_gender['Weight'])
+regressor.fit(X_train, y_train)
 
 print(regressor.intercept_) #Prints the y-intercept of the regression line
 print(regressor.coef_) #Prints the slope of the regression line
 
 y_pred = regressor.predict(X_test)
+y_train_pred = regressor.predict(X_train)
 
 #Plot 2D with seaborn
 sns.set()
@@ -55,27 +56,32 @@ fittedY=results_formula.predict(exog=onlyX)
 # convert the predicted result in an array
 fittedY=np.array(fittedY)
 
-fig = plt.figure()
+fig = plt.figure(figsize = (10,10))
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(df_gender['Height'],df_gender['Weight'],df_gender['Gender_Male'],c='red', marker='o', alpha=0.5)
-ax.plot_surface(x_surf,y_surf,fittedY.reshape(x_surf.shape), color='b', alpha=0.3)
+ax.plot_surface(x_surf,y_surf,fittedY.reshape(x_surf.shape), cmap = 'viridis', alpha=0.5)
 ax.set_xlabel('Height')
 ax.set_ylabel('Weight')
 ax.set_zlabel('Gender')
 plt.show()
 
+print("Test set")
 print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
 print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
 print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+print("Train set")
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_train, y_train_pred))  
+print('Mean Squared Error:', metrics.mean_squared_error(y_train, y_train_pred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_train, y_train_pred)))
 
 def main():
     gender = (input("What is your gender (male or female)?"))
     given_height = float(input('What is your height in inches?'))
     if gender == "male":
-        predicted_weight = regressor.intercept_ + regressor.coef_[0] * given_height + (regressor.coef_[1] * 1)
+        predicted_weight = regressor.intercept_ + regressor.coef_[0,0] * given_height + (regressor.coef_[0,1] * 1)
         print("I guess that your weight is: " + str(predicted_weight) + " lbs!")
     elif gender == "female":
-        predicted_weight = regressor.intercept_ + regressor.coef_[0] * given_height + (regressor.coef_[1] * 0)
+        predicted_weight = regressor.intercept_ + regressor.coef_[0,0] * given_height + (regressor.coef_[0,1] * 0)
         print("I guess that your weight is: " + str(predicted_weight) + " lbs!")
     elif gender != "male" or "female":
         print("Please enter valid gender. Make sure letters are all lowercase") 
